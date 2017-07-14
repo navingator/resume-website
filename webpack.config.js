f4956d8794d9789d6e763e1bb682980a0c3e00f7
Navin Pathak <np4navin@gmail.com>
2017-07-14 14:45:29 -0400

    Added webpack and materialize configuration

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		vendor: 'materialize-loader!./scripts/materialize/materialize.config.js',
+		main: './src/main.js'
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].bundle.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: "commons",
+			filename: "commons.js"
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.optimize.ModuleConcatenationPlugin() // scope hoisting
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				use: [{
+					loader: 'babel-loader',
+					options: { presets: ['es2015']}
+				}]
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 80000,
+						mimetype: "application/font-woff"
+					}
+				}]
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				use: [{ loader: "file-loader" }]
+			}
+		]
+	},
+
+	devServer: {
+		hot: true,
+		contentBase: path.resolve(__dirname, './src'),
+		publicPath: '/'
+	}
+};
