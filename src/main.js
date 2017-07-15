3e25093de2c025ccbf80f9dd58de1d888f6e392c
Navin Pathak <np4navin@gmail.com>
2017-07-15 18:06:46 -0400

    Added scrollspy and footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './css/styles.css';
 
 (function() {
-	$(".dropdown-button").dropdown();
-	$(".button-collapse").sideNav();
+	$(document).ready(function(){
+		$(".dropdown-button").dropdown();
+		$(".button-collapse").sideNav();
+
+		const toc = $('.table-of-contents');
+		toc.pushpin({ top: toc.offset().top });
+		$('.scrollspy').scrollSpy();
+	});
 })();
