2f05a30d8f44e21edbd58e5dcbb3954d65f8ba6b
Navin Pathak <np4navin@gmail.com>
2017-07-18 14:49:09 -0400

    Added google analytics and refacted initialization script

---
+++ src/js/init.js
@@ -0,0 +1,17 @@
+
+// Materialize jQuery
+(function() {
+	$(document).ready(function(){
+		$('.dropdown-button').dropdown({
+			constrainWidth: false,
+			hover: false,
+			belowOrigin: true,
+			alignment: 'right'
+		});
+		$('.button-collapse').sideNav();
+
+		const toc = $('.table-of-contents');
+		toc.pushpin({ top: toc.offset().top });
+		$('.scrollspy').scrollSpy();
+	});
+})();
--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
 import './css/styles.css';
 import './favicon.ico';
 
-(function() {
-	$(document).ready(function(){
-		$('.dropdown-button').dropdown({
-			constrainWidth: false,
-			hover: false,
-			belowOrigin: true,
-			alignment: 'right'
-		});
-		$('.button-collapse').sideNav();
-
-		const toc = $('.table-of-contents');
-		toc.pushpin({ top: toc.offset().top });
-		$('.scrollspy').scrollSpy();
-	});
-})();
+import './js/init';
+import './js/google-analytics';
