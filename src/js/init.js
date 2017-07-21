bc953a60ec4a73a0944438bc54121a44a83380fe
Navin Pathak <np4navin@gmail.com>
2017-07-21 14:09:24 -0400

    Added expansion animation for resume items.

--- src/js/init.js
+++ src/js/init.js
@@ -13,5 +13,44 @@
 		const toc = $('.table-of-contents');
 		toc.pushpin({ top: toc.offset().top });
 		$('.scrollspy').scrollSpy();
+
+		$('.resume-item > .card').click(toggleActiveCard);
 	});
+
+	function toggleActiveCard() {
+		const card = $(this);
+		const contentWrap = card.find('.card-content');
+		const content = card.find('.resume-main-content');
+		const prevCss = content.attr('style'); // cache style attribute to set later
+		const heights = []; // array that contains initial and final heights 
+
+		// cache initial card height for setting later
+		heights[0] = card.height();
+
+		// Get the new card height
+		card.css({ height: 'auto' });
+		if (card.hasClass('active')) {
+			content.css({ display: 'none' });
+		} else {
+			content.css({ display: 'block' });
+		}
+		heights[1] = card.height(); // get calculated card height
+
+		// Reset css
+		if (prevCss) { content.css(prevCss); }
+			else { content.removeAttr('style'); }
+		card.css({ height: heights[0] }); // set the card height - required for animations
+		this.offsetHeight; // HACK: trigger html reflow
+		card.css({ height: heights[1] }); // set the new card height
+
+		// set timeouts
+		let timeout = 250; // must match CSS Timeout
+		if (card.hasClass('active')) {
+			timeout = 0;
+		}
+		
+		setTimeout(function() {
+			card.toggleClass('active');
+		}, timeout);
+	}
 })(jQuery);
